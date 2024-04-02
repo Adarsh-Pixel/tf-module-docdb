1c4a8fa49432c9ebc5cf1c41ec71837d292f3ef4
unknown <kumaradarsh921@gmail.com>
2024-04-02 12:13:59 +0530

    Git Auto push

--- cluster.tf
+++ cluster.tf
@@ -1,21 +1,21 @@
-resource "aws_docdb_cluster" "docdb" {
-  cluster_identifier      = "roboshop-${var.ENV}-docdb"
-  engine                  = "docdb"
-  master_username         = "admin1"
-  master_password         = "roboshop1"
-  skip_final_snapshot     = true            # value will be false in production. In lab, we will be using true
-  db_subnet_group_name    = aws_docdb_subnet_group.docdb.name
-}
+# resource "aws_docdb_cluster" "docdb" {
+#   cluster_identifier      = "roboshop-${var.ENV}-docdb"
+#   engine                  = "docdb"
+#   master_username         = "admin1"
+#   master_password         = "roboshop1"
+#   skip_final_snapshot     = true            # value will be false in production. In lab, we will be using true
+#   db_subnet_group_name    = aws_docdb_subnet_group.docdb.name
+# }
 
 
-# Create DocDB subnet group
+# # Create DocDB subnet group
 
-resource "aws_docdb_subnet_group" "docdb" {
-  name       = "roboshop-${var.ENV}-docdb-subnet-group"
-  subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
+# resource "aws_docdb_subnet_group" "docdb" {
+#   name       = "roboshop-${var.ENV}-docdb-subnet-group"
+#   subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
 
-  tags = {
-    Name = "roboshop-${var.ENV}-docdb-subnet-group"
-  }
-}
+#   tags = {
+#     Name = "roboshop-${var.ENV}-docdb-subnet-group"
+#   }
+# }
 

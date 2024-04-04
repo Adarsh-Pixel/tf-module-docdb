807248a20614344c7361f8ea912daf29ad0ccd08
unknown <kumaradarsh921@gmail.com>
2024-04-04 16:46:31 +0530

    Git Auto push

--- cluster.tf
+++ cluster.tf
@@ -10,10 +10,10 @@ resource "aws_docdb_cluster" "docdb" {
 # Creates DocDB instances and adds to the cluster
 
 resource "aws_docdb_cluster_instance" "cluster_instances" {
-  count              = 2
-  identifier         = "docdb-cluster-demo-${count.index}"
-  cluster_identifier = aws_docdb_cluster.default.id
-  instance_class     = "db.r5.large"
+  count              = 1
+  identifier         = "roboshop-${var.ENV}-docdb"
+  cluster_identifier = aws_docdb_cluster.docdb.id
+  instance_class     = "db.t3.medium"
 }
 
 

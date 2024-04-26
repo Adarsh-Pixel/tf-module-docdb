6933c2d1c73608bab20b4997cc4300ba5280f63e
unknown <kumaradarsh921@gmail.com>
2024-04-26 18:09:07 +0530

    Git Auto push

--- data.tf
+++ data.tf
@@ -7,3 +7,18 @@ data "terraform_remote_state" "vpc" {
     region = "us-east-1"
   }
 }
+
+# Fetches the information of the secret
+
+data "aws_secretsmanager_secret" "secrets" {
+  name = "robot/secrets"
+}
+
+# fetches the secrets version from the above server
+resource "aws_secretsmanager_secret_version" "secret_version" {
+  secret_id     = data.aws_secretsmanager_secret.secret_version.id
+}
+
+output "example" {
+  value = jsondecode(aws_secretsmanager_secret_version.secret_version.secret_string)["DOCDB_USERNAME"]
+}
